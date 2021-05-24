6ef81beeaec5d774b16bd404cd886b2594b66ba3
guddi yadav <69110026+guddi991@users.noreply.github.com>
2021-05-24 11:09:20 +0530

    Add files via upload

---
+++ DAY2/BITWISE.java
@@ -0,0 +1,22 @@
+ <----------------------------------BITBISE AND & OR OPERATION---------------------------------------------//
+
+public class DAY_2{
+    public static void main(String[] args){
+        int a = 25; 
+        int b = 50;
+        int c = a&b;
+        int d = a|b;
+        int f = c&d;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(f);
+
+
+    }
+}
+
+
+
+
+
+
